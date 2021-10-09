6a86e4d66927eb5cb4d6efd9199ebd4a8c9dd1e2
TBlais10 <blaislighting@gmail.com>
2021-10-09 11:47:45 -0400

    Updates made once methods were built out. Methods have been tested with exception of getChar and found issue with not redefining the input field inside methods causing infinate loop or immediate completion. Further testing must be done to be satisfied with the method.

--- src/com/careerdevs/CLI.java
+++ src/com/careerdevs/CLI.java
@@ -1,27 +1,72 @@
 package com.careerdevs;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class CLI {
 
-    Scanner scanner = new Scanner(System.in);
+    private static Scanner scanner = new Scanner(System.in);
 
 
     public static String getString(String statement) {
         String input = "";
-        System.out.print("Input: ");
+        try {
+            while (true) {
+                System.out.print("Input: ");
+                input = scanner.nextLine();
+                if (input.length() == 0) {
+                    System.out.println("Input contains no characters! Please try again");
+                }
+                else if (!input.equalsIgnoreCase(statement)){
+                    System.out.println("Statement does not match given option. Please try again.");
+                }
+                else {
+                    break;
+                }
+            }
+        } catch (InputMismatchException exception) {
+            System.out.println("Incorrect input! Please try again");
+        } catch (Exception exception) {
+            System.out.println("Unknown error. Please try again. If this message appears again please contact the developers about this issue.");
+        }
         return input;
     }
 
     public static int getNum(int min, int max) {
         int input = 0;
-        System.out.print("Input: ");
+        try {
+            while (true) {
+                System.out.print("Input: ");
+                input = scanner.nextInt();
+                if (input > max || input < min) {
+                    System.out.println("Input out of range! Please try again with a number between " + min + " and " + max + ".");
+                } else {
+                    break;
+                }
+            }
+        } catch (InputMismatchException exception) {
+            System.out.println("Incorrect input! Please try again with a set of numbers.");
+        } catch (Exception exception) {
+            System.out.println("Unknown error. Please try again. If this message appears again please contact the developers about this issue.");
+        }
         return input;
     }
 
     public static char getChar(char statement) {
         char input = 0;
-        System.out.print("Input: ");
+        try {
+            while (true) {
+                System.out.print("Input: ");
+                input = scanner.next().charAt(0);
+                break;
+            }
+            return input;
+        } catch (InputMismatchException exception){
+            System.out.println("Incorrect input! Please try again.");
+        }
+        catch (Exception exception){
+            System.out.println("Unknown error. Please try again. If this message appears again please contact the developers about this issue.");
+        }
         return input;
     }
 
@@ -34,6 +79,7 @@ public class CLI {
         String input = "";
         while (true) {
             System.out.print("Input: ");
+            input = scanner.nextLine();
             if (!input.equals("")) {
                 System.out.println("Please press enter to continue.");
             } else {
@@ -44,8 +90,15 @@ public class CLI {
         return input;
     }
 
-    public static void createSeperator (String seperator, int repeat){
+    public static void createSeperator(String seperator, int repeat) {
         System.out.println(seperator.repeat(repeat));
+    }
+
+    public static void main(String[] args) {
+//        readEnter();
+//        getNum(1, 2);
+//        createSeperator("- ", 20);
+        getString("Yes");
     }
 
 }
