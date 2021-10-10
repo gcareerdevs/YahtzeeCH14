5fbdc67e232cb1ffbbbf523e195e9550a0563b73
enjee1 <22105805+enjee1@users.noreply.github.com>
2021-10-09 20:45:24 -0400

    Initial commit

--- src/com/careerdevs/Game.java
+++ src/com/careerdevs/Game.java
@@ -1,4 +1,12 @@
 package com.careerdevs;
 
+import java.util.ArrayList;
+
 public class Game {
+    private ArrayList<Player> players;
+    private ArrayList<Player> winners;
+    private final byte NUM_ROUNDS = 13;
+    private final byte NUM_DICE = 5;
+    private final byte NUM_TURNS= 3;
+
 }
