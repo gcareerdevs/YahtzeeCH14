c1962e41b8b57487614a50421f863cf0a44f5cd5
Jeff Mitsou <shinetachi88@gmail.com>
2021-10-11 13:43:41 -0400

    Created Skeleton for Menu Class

--- src/com/careerdevs/Menu.java
+++ src/com/careerdevs/Menu.java
@@ -1,4 +1,17 @@
 package com.careerdevs;
 
 public class Menu {
+
+    public static String welcome_msg;
+    public static String rules;
+
+    public static String showWelcome(){
+        return welcome_msg;
+    }
+
+    public static String showRules(){
+        return rules;
+    }
+
+
 }
