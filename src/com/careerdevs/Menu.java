69b3afc8afb68b42daedc87df64ccada45b1cffe
Jeff Mitsou <shinetachi88@gmail.com>
2021-10-12 18:20:37 -0400

    Added Start Content

--- src/com/careerdevs/Menu.java
+++ src/com/careerdevs/Menu.java
@@ -15,6 +15,29 @@ public class Menu {
 
     public static void start(){
 
+        System.out.println("*************************");
+        System.out.println(" Welcome to Yahtzee ");
+        System.out.println("*************************");
+
+        System.out.println("\nWelcome lets get started.");
+
+        System.out.println("\nPlease select a option");
+        System.out.println("(1) Start Game.\n(2) Exit");
+        System.out.print("\nChoice: ");
+        int choice = CLI.getNum(1,2);
+        int numPlayers = 0;
+
+        if (choice == 1) {
+            System.out.println("\nGreat, How many people will me playing?");
+            System.out.print("\nNumber of Players: ");
+            numPlayers = CLI.getNum(2, 10);
+        }else if (choice == 2){
+            System.out.println("Okay maybe we can play next time, come back soon");
+        }
+
+        System.out.println("\nOkay so now lets start the game.");
+        Game newGame = new Game(numPlayers);
+
     }
 
 
