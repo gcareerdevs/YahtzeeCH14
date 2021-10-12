8e4e709c74a04324c41c6172639f8d690c94c18f
Richies Luong <richiesluong.student@careerdevs.com>
2021-10-11 22:42:12 -0400

    Player Class

--- src/com/careerdevs/Player.java
+++ src/com/careerdevs/Player.java
@@ -1,4 +1,21 @@
 package com.careerdevs;
 
 public class Player {
+
+    public String name;
+    public Scorecard scorecard;
+    public Hand hand;
+
+    public Player(String name, Scorecard scorecard) {
+        this.name = name;
+        this.scorecard = scorecard;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setHand(Hand hand) {
+        this.hand = hand;
+    }
 }
