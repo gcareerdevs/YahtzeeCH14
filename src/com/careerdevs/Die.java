55eebc846f36f32745eab1bf4efbedf97190a300
enjee1 <22105805+enjee1@users.noreply.github.com>
2021-10-14 21:48:02 -0400

    Refactored Die constructors, add Random class object to roll()

--- src/com/careerdevs/Die.java
+++ src/com/careerdevs/Die.java
@@ -1,4 +1,25 @@
 package com.careerdevs;
 
 public class Die {
+    private final Random myRand;
+    private final int sides;
+    private int value;
+
+    public Die() {
+        this(6);
+    }
+
+    public Die (int sides) {
+        this.sides = sides;
+        myRand = new Random();
+    }
+
+    public int roll() {
+        value = (myRand.nextInt(sides) + 1);
+        return value;
+    }
+
+    public int getValue() {
+        return value;
+    }
 }
