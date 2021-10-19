bef2df241c321907c619dc9225cd1df1f1ad9621
Nathan1006 <nrpetit@outlook.com>
2021-10-18 20:42:32 -0400

    Finishing touches onto menu class for language

--- src/com/careerdevs/Menu.java
+++ src/com/careerdevs/Menu.java
@@ -2,43 +2,44 @@ package com.careerdevs;
 
 public class Menu {
 
-    public static String welcome_msg;
-    public static String rules;
+    public static String welcome_msg = "Hello and welcome to Yahtzee! Lets get started!";
+    public static String rules = "Before you play Yahtzee, lets help you understand it's basic rules: \n" +
+            "1. Players will take turns rolling the game dice for a total of 13 rounds \n" +
+            "2. Each player will have at maximum 3 attempts to roll their dice during each of their turns, however not all 3 rolls have to be used \n" +
+            "3. Each player will have a scorecard they must fill out each turn which will determine their final score. Scoring fields can be filled out with a 0 if chosen";
 
-    public static String showWelcome(){
-        return welcome_msg;
+    public static void showWelcome() {
+        CLI.createSeperator("*" , 20);
+        System.out.println(welcome_msg);
+        CLI.createSeperator("*" , 20);
     }
 
-    public static String showRules(){
-        return rules;
+    public static void showRules() {
+        System.out.println(rules);
     }
 
-    public static void start(){
+    public static void start() {
 
-        System.out.println("*************************");
-        System.out.println(" Welcome to Yahtzee ");
-        System.out.println("*************************");
+        showWelcome();
 
-        System.out.println("\nWelcome lets get started.");
-
-        System.out.println("\nPlease select a option");
-        System.out.println("(1) Start Game.\n(2) Exit");
-        System.out.print("\nChoice: ");
-        int choice = CLI.getNum(1,2);
         int numPlayers = 0;
 
-        if (choice == 1) {
-            System.out.println("\nGreat, How many people will me playing?");
-            System.out.print("\nNumber of Players: ");
-            numPlayers = CLI.getNum(2, 10);
-        }else if (choice == 2){
-            System.out.println("Okay maybe we can play next time, come back soon");
+        while (true) {
+            System.out.println("\nPlease select a option");
+            System.out.println("(1) Start Game\n(2) Display Rules \n(3) Exit");
+            int choice = CLI.getNum(1, 3);
+            if (choice == 1) {
+                System.out.println("\nGreat, How many people will me playing?");
+                System.out.print("\nNumber of Players: ");
+                numPlayers = CLI.getNum(2, 10);
+                System.out.println("\nOkay so now lets start the game.");
+                new Game(numPlayers);
+            } else if (choice == 2) {
+                showRules();
+                CLI.readEnter();
+            } else if (choice == 3) {
+                CLI.exit();
+            }
         }
-
-        System.out.println("\nOkay so now lets start the game.");
-        Game newGame = new Game(numPlayers);
-
     }
-
-
 }
