40dda590bab1c6e8560dc9b770430ea99d2968e6
enjee1 <22105805+enjee1@users.noreply.github.com>
2021-10-19 21:34:53 -0400

    Started preliminary ScoreCard class and Combination subclass code skeletons

--- src/com/careerdevs/Scorecard.java
+++ src/com/careerdevs/Scorecard.java
@@ -1,8 +1,33 @@
 package com.careerdevs;
 
+import java.util.HashMap;
+
 public class Scorecard {
+    private int grandTotal;
+    private int upperTotal;
+    private int lowerTotal;
+    private int bonus;
+    private HashMap<String, Combination> upperCard;
+    private HashMap<String, Combination> lowerCard;
+
+    public Scorecard() {
+        grandTotal = 0;
+        upperTotal = 0;
+        lowerTotal = 0;
+        bonus = 0;
+        upperCard = new HashMap<>();
+        lowerCard = new HashMap<>();
+    }
+
+    class Combination {
+        public Combination() {
+
+        }
+    }
 
     public void testScore() {
         System.out.println("Scoring");
     }
 }
+
+
