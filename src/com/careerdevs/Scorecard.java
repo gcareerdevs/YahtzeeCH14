79a2e48e8783a64cb39f7fd92042f428e5c41f01
enjee1 <22105805+enjee1@users.noreply.github.com>
2021-10-21 20:27:20 -0400

    Added initializeUpper/Lower () methods

--- src/com/careerdevs/Scorecard.java
+++ src/com/careerdevs/Scorecard.java
@@ -17,6 +17,8 @@ public class Scorecard {
         bonus = 0;
         upperCard = new HashMap<>();
         lowerCard = new HashMap<>();
+        initializeUpperCard();
+        initializeLowerCard();
     }
 
     class Combo {
@@ -26,6 +28,42 @@ public class Scorecard {
             this.score = 0;
             this.isFilled = false;
         }
+    }
+
+    private void initializeUpperCard() {
+        Combo combo1 = new Combo();
+        Combo combo2 = new Combo();
+        Combo combo3 = new Combo();
+        Combo combo4 = new Combo();
+        Combo combo5 = new Combo();
+        Combo combo6 = new Combo();
+
+
+        upperCard.put("Aces", combo1 );
+        upperCard.put("Twos", combo2);
+        upperCard.put("Threes", combo3);
+        upperCard.put("Fours", combo4);
+        upperCard.put("Fives", combo5);
+        upperCard.put("Sixes", combo6);
+    }
+
+    private void initializeLowerCard() {
+        Combo combo1 = new Combo();
+        Combo combo2 = new Combo();
+        Combo combo3 = new Combo();
+        Combo combo4 = new Combo();
+        Combo combo5 = new Combo();
+        Combo combo6 = new Combo();
+        Combo combo7 = new Combo();
+
+
+        upperCard.put("Three of a Kind", combo1 );
+        upperCard.put("Four of a Kind", combo2);
+        upperCard.put("Small Straight", combo3);
+        upperCard.put("Large Straight", combo4);
+        upperCard.put("Full House", combo5);
+        upperCard.put("Yahtzee", combo6);
+        upperCard.put("Chance", combo7);
     }
 
     public void testScore() {
