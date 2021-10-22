a4dbea64aa716ea169ed204eec75d06155d25248
enjee1 <22105805+enjee1@users.noreply.github.com>
2021-10-22 15:24:59 -0400

    Revised Scorecard methods, added ComboKey enum

--- src/com/careerdevs/Scorecard.java
+++ src/com/careerdevs/Scorecard.java
@@ -1,5 +1,6 @@
 package com.careerdevs;
 
+import java.util.Date;
 import java.util.HashMap;
 
 public class Scorecard {
@@ -7,12 +8,40 @@ public class Scorecard {
     private int upperTotal;
     private int lowerTotal;
     private int bonus;
+    private final int MAX_SCORE_NAME_LENGTH = 17;
     private HashMap<String, Combo> card;
-    private static final String[] COMBO_NAMES = {
+    private enum ComboKey {
+        ACE ("Aces"),
+        TWO ("Twos"),
+        THREE ("Threes"),
+        FOUR ("Fours"),
+        FIVE ("Fives"),
+        SIX ("Sixes"),
+        TOAK ("Three of a Kind"),
+        FOAK ("Four of a Kind"),
+        SMSTR ("Small Straight"),
+        LRGSTR ("Large Straight"),
+        FLLHSE ("Full House"),
+        YHTZE ("Yahtzee"),
+        CHNC ("Chance");
+
+        private String enumName;
+
+        ComboKey(String name) {
+            enumName = name;
+        }
+        
+        private String getName() {
+            return enumName;
+        }
+
+    };
+
+    /*private static final String[] COMBO_NAMES = {
             "Aces", "Twos", "Threes", "Fours", "Fives", "Sixes",
             "Three of a Kind", "Four of a Kind", "Small Straight", "Large Straight",
             "Full House", "Yahtzee", "Chance"
-    };
+    };*/
 
 
     public Scorecard() {
@@ -25,27 +54,44 @@ public class Scorecard {
     }
 
     class Combo {
-        public int score;
-        public boolean isFilled;
-        public boolean isUpper;
+        private int score;
+        private String name;
+        private boolean isFilled;
+        private boolean isUpper;
 
-
-        public Combo(boolean isUpper) {
+        public Combo(String name, boolean isUpper) {
+            this.name = name;
             this.score = 0;
             this.isFilled = false;
             this.isUpper = isUpper;
         }
+
+        public String getName() {
+            return name;
+        }
+        @Override
+        public String toString() {
+            String scoreSpacer = (" ".repeat(MAX_SCORE_NAME_LENGTH - name.length()));
+            return name + scoreSpacer + " || Score: " + score + " Used " + (isFilled ? "[X]" : "[ ]");
+        }
     }
 
     private void initializeCard() {
-
-        for (int i = 0; i < COMBO_NAMES.length ; i++) {
-            if (i < 6) {
-                card.put(COMBO_NAMES[i], new Combo(true));
+        for (ComboKey key : ComboKey.values()) {
+            if (key.ordinal() < 6) {
+                card.put(key.toString(), new Combo(key.getName(),true));
             } else {
-                card.put(COMBO_NAMES[i], new Combo(false));
+                card.put(key.toString(), new Combo(key.getName(),false));
             }
         }
+
+        /*for (int i = 0; i < ComboName.values().length; i++) {
+            if (i < 6) {
+                card.put(ComboName.values()[i].toString(), new Combo(true));
+            } else {
+                card.put(ComboName.values()[i].toString(), new Combo(false));
+            }
+        }*/
     }
 
     private HashMap<String, Combo> findAvailableCombos() {
@@ -60,6 +106,19 @@ public class Scorecard {
         return availableCombos;
 
 
+    }
+
+    @Override
+    public String toString() {
+        String output = "";
+
+        for (ComboKey c : ComboKey.values()) {
+            output += (card.get(c.toString())) + "\n";
+        }
+        /*for (int i = 0; i < card.size(); i++) {
+            output += card.values();
+        }*/
+        return output;
     }
     public void testScore() {
         System.out.println("Scoring");
